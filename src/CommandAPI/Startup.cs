d868a6b11639a3fda67fba76819052ddb95e2fef
milosbgnp <69404070+milosbgnp@users.noreply.github.com>
2020-08-15 01:14:46 +0200

    Add files via upload

---
+++ src/CommandAPI/Startup.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using CommandAPI.Models;
+using Npgsql;
+
+namespace CommandAPI
+{
+    public class Startup
+    {
+        public IConfiguration Configuration {get;}
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var builder = new NpgsqlConnectionStringBuilder();
+            builder.ConnectionString = Configuration.GetConnectionString("PostgreSqlConnection");
+            builder.Username = Configuration["UserID"];
+            builder.Password = Configuration["Password"];
+            services.AddDbContext<CommandContext>(opt => opt.UseNpgsql(builder.ConnectionString));
+
+            
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(opt => {
+                    opt.Audience = Configuration["ResourceId"];
+                    opt.Authority = $"{Configuration["Instance"]}{Configuration["TenantId"]}";
+                });
+            
+            services.AddControllers();
+        }
+
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, CommandContext context)
+        {
+            context.Database.Migrate();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+               endpoints.MapControllers();
+            });
+        }
+    }
+}
